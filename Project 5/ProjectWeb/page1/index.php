6457a1ce098e16fc145dca3f3ee6d8ea33efb0a5
Yongki <yongkitanu@gmail.com>
2019-10-22 14:16:28 +0700

    Update index.php

--- Project 5/ProjectWeb/page1/index.php
+++ Project 5/ProjectWeb/page1/index.php
@@ -6,11 +6,18 @@
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>      
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <style>
+        .wht{
+          background: red;
+          height: 100px;
+        }
+      </style>
     </head>
     <body>
-      <section id="nav-bar" class="nav-bar">
-         
+      <section id="whyUph" class="nav-bar">
+         <div class="wht">gshdgahdghagdgahsgdhasgdhgasgdhjgagsdgahgs</div>
       </section>
+      
     </body>
   </html>
   <script type="text/javascript" src="js/materialize.min.js"></script>
