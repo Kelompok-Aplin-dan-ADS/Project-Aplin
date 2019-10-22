e3bf583ad3ea19531dcae6483b3f83e5385dd236
fabiansuryajaya <54833728+fabiansuryajaya@users.noreply.github.com>
2019-10-22 14:20:28 +0700

    Update index.php

--- Project 5/ProjectWeb/page1/index.php
+++ Project 5/ProjectWeb/page1/index.php
@@ -1,16 +1,32 @@
  <!DOCTYPE html>
-  <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>      
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
-    <body>
-      <section id="nav-bar" class="nav-bar">
-         
-      </section>
-    </body>
-  </html>
+    <html>
+          <head>
+              <!--Import Google Icon Font-->
+              <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+              <!--Import materialize.css-->
+              <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>      
+              <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          </head>
+          <body>
+            <section id="nav-bar" class="nav-bar">
+              <ul id="dropdown1" class="dropdown-content">
+                <li><a href="#!">one</a></li>
+                <li><a href="#!">two</a></li>
+                <li class="divider"></li>
+                <li><a href="#!">three</a></li>
+              </ul>
+              <nav>
+                <div class="nav-wrapper">
+                  <a href="#!" class="brand-logo">Logo</a>
+                  <ul class="right hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <!-- Dropdown Trigger -->
+                    <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+                  </ul>
+                </div>
+              </nav>
+            </section>
+        </body>
+    </html>
   <script type="text/javascript" src="js/materialize.min.js"></script>
